485f79a08589d7933ace74e17493cb8aba9ece30
mohamedibrahim4 <mohamedatlm297@gmail.com>
2023-10-01 01:03:20 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace testWindowsblp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtresult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfileName = new System.Windows.Forms.TextBox();
+            this.lblfilename = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblTextRows = new System.Windows.Forms.Label();
+            this.lblTextColumns = new System.Windows.Forms.Label();
+            this.lblResultColumn = new System.Windows.Forms.Label();
+            this.lblResultRows = new System.Windows.Forms.Label();
+            this.rbtntextRows = new System.Windows.Forms.ComboBox();
+            this.rbtntextColumns = new System.Windows.Forms.ComboBox();
+            this.rbtnResultRows = new System.Windows.Forms.ComboBox();
+            this.rbtnResultColumns = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(252, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GET SPY:US";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtresult
+            // 
+            this.txtresult.Location = new System.Drawing.Point(193, 95);
+            this.txtresult.Multiline = true;
+            this.txtresult.Name = "txtresult";
+            this.txtresult.Size = new System.Drawing.Size(525, 98);
+            this.txtresult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SPY:US";
+            // 
+            // txtfileName
+            // 
+            this.txtfileName.Location = new System.Drawing.Point(193, 260);
+            this.txtfileName.Multiline = true;
+            this.txtfileName.Name = "txtfileName";
+            this.txtfileName.Size = new System.Drawing.Size(525, 34);
+            this.txtfileName.TabIndex = 3;
+            // 
+            // lblfilename
+            // 
+            this.lblfilename.AutoSize = true;
+            this.lblfilename.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfilename.Location = new System.Drawing.Point(12, 262);
+            this.lblfilename.Name = "lblfilename";
+            this.lblfilename.Size = new System.Drawing.Size(155, 18);
+            this.lblfilename.TabIndex = 4;
+            this.lblfilename.Text = "Exceel Sheet Name";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(281, 320);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(250, 46);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.Save);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(663, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Clear);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // lblTextRows
+            // 
+            this.lblTextRows.AutoSize = true;
+            this.lblTextRows.Location = new System.Drawing.Point(23, 134);
+            this.lblTextRows.Name = "lblTextRows";
+            this.lblTextRows.Size = new System.Drawing.Size(34, 13);
+            this.lblTextRows.TabIndex = 7;
+            this.lblTextRows.Text = "Rows";
+            // 
+            // lblTextColumns
+            // 
+            this.lblTextColumns.AutoSize = true;
+            this.lblTextColumns.Location = new System.Drawing.Point(96, 134);
+            this.lblTextColumns.Name = "lblTextColumns";
+            this.lblTextColumns.Size = new System.Drawing.Size(47, 13);
+            this.lblTextColumns.TabIndex = 9;
+            this.lblTextColumns.Text = "Columns";
+            // 
+            // lblResultColumn
+            // 
+            this.lblResultColumn.AutoSize = true;
+            this.lblResultColumn.Location = new System.Drawing.Point(400, 205);
+            this.lblResultColumn.Name = "lblResultColumn";
+            this.lblResultColumn.Size = new System.Drawing.Size(47, 13);
+            this.lblResultColumn.TabIndex = 13;
+            this.lblResultColumn.Text = "Columns";
+            // 
+            // lblResultRows
+            // 
+            this.lblResultRows.AutoSize = true;
+            this.lblResultRows.Location = new System.Drawing.Point(341, 205);
+            this.lblResultRows.Name = "lblResultRows";
+            this.lblResultRows.Size = new System.Drawing.Size(34, 13);
+            this.lblResultRows.TabIndex = 12;
+            this.lblResultRows.Text = "Rows";
+            // 
+            // rbtntextRows
+            // 
+            this.rbtntextRows.FormattingEnabled = true;
+            this.rbtntextRows.Location = new System.Drawing.Point(26, 153);
+            this.rbtntextRows.Name = "rbtntextRows";
+            this.rbtntextRows.Size = new System.Drawing.Size(60, 21);
+            this.rbtntextRows.TabIndex = 16;
+            // 
+            // rbtntextColumns
+            // 
+            this.rbtntextColumns.FormattingEnabled = true;
+            this.rbtntextColumns.Location = new System.Drawing.Point(99, 153);
+            this.rbtntextColumns.Name = "rbtntextColumns";
+            this.rbtntextColumns.Size = new System.Drawing.Size(60, 21);
+            this.rbtntextColumns.TabIndex = 17;
+            // 
+            // rbtnResultRows
+            // 
+            this.rbtnResultRows.FormattingEnabled = true;
+            this.rbtnResultRows.Location = new System.Drawing.Point(332, 222);
+            this.rbtnResultRows.Name = "rbtnResultRows";
+            this.rbtnResultRows.Size = new System.Drawing.Size(60, 21);
+            this.rbtnResultRows.TabIndex = 18;
+            // 
+            // rbtnResultColumns
+            // 
+            this.rbtnResultColumns.FormattingEnabled = true;
+            this.rbtnResultColumns.Location = new System.Drawing.Point(403, 221);
+            this.rbtnResultColumns.Name = "rbtnResultColumns";
+            this.rbtnResultColumns.Size = new System.Drawing.Size(60, 21);
+            this.rbtnResultColumns.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbtnResultColumns);
+            this.Controls.Add(this.rbtnResultRows);
+            this.Controls.Add(this.rbtntextColumns);
+            this.Controls.Add(this.rbtntextRows);
+            this.Controls.Add(this.lblResultColumn);
+            this.Controls.Add(this.lblResultRows);
+            this.Controls.Add(this.lblTextColumns);
+            this.Controls.Add(this.lblTextRows);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblfilename);
+            this.Controls.Add(this.txtfileName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtresult);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Bloomberg Data";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtresult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfileName;
+        private System.Windows.Forms.Label lblfilename;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label lblTextColumns;
+        private System.Windows.Forms.Label lblTextRows;
+        private System.Windows.Forms.Label lblResultColumn;
+        private System.Windows.Forms.Label lblResultRows;
+        private System.Windows.Forms.ComboBox rbtntextRows;
+        private System.Windows.Forms.ComboBox rbtntextColumns;
+        private System.Windows.Forms.ComboBox rbtnResultColumns;
+        private System.Windows.Forms.ComboBox rbtnResultRows;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Windows.Forms;
+//using System.Text.UTF8Encoding;
+using Microsoft.Office.Interop;
+namespace testWindowsblp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (txtresult.Text=="")
+            {
+                lblfilename.Visible = false;
+                txtfileName.Visible = false;
+                btnSave.Visible = false;
+                for (int i = 1; i <= 25; i++)
+                {
+                    string[] numbers = { i.ToString() };
+                    rbtntextRows.Items.AddRange(numbers);
+                    rbtntextColumns.Items.AddRange(numbers);
+                    rbtnResultRows.Items.AddRange(numbers);
+                    rbtnResultColumns.Items.AddRange(numbers);
+
+                }
+                lblTextRows.Visible = false;
+                lblTextColumns.Visible = false;
+                lblResultRows.Visible = false;
+                lblResultColumn.Visible = false;
+
+                rbtntextRows.Visible = false;
+                rbtntextColumns.Visible = false;
+                rbtnResultRows.Visible = false;
+                rbtnResultColumns.Visible = false;
+
+            }
+        }
+        //object request = new object();
+        String response;
+        HtmlDocument html;
+        string website;
+        float price;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            website = "https://www.bloomberg.com/quote/SPY:US";
+            dynamic  request = Microsoft.VisualBasic.Interaction.CreateObject("MSXML2.ServerXMLHTTP");
+            request.open("Get", website, false);
+            request.SetRequestHeader("if-modified-Since", "Sat, 1 Jan 2000 00:00:00 GMT");
+            request.Send();
+            
+            if (request.Status == 200)
+            {
+
+                response = request.responseText;
+                int StartindexPattern = response.LastIndexOf("<span class=\"priceText__06f600fa3e\">");
+
+                //string gg ="changeAbsolute__cd26c98843 negative__d4d87cba99 animatedBackground__e12b56f780";
+                //int j = gg.Length;
+
+                //int StartindexPattern2 = response.LastIndexOf("<span class=\"changeAbsolute__cd26c98843 negative__d4d87cba99 animatedBackground__e12b56f780\">");
+                //string specificNumber2 = response.Substring(StartindexPattern2 + 93, 8);
+
+                string specificNumber = response.Substring(StartindexPattern + 36, 8);
+                txtresult.Text = specificNumber ;
+            }
+            else
+            {
+                return;
+            }
+            if (txtresult.Text !="")
+            {
+                lblfilename.Visible = true;
+                txtfileName.Visible = true;
+                btnSave.Visible = true;
+
+                lblTextRows.Visible = true;
+                lblTextColumns.Visible = true;
+                lblResultRows.Visible = true;
+                lblResultColumn.Visible = true;
+
+                rbtntextRows.Visible = true;
+                rbtntextColumns.Visible = true;
+                rbtnResultRows.Visible = true;
+                rbtnResultColumns.Visible = true;
+            }
+            //CreateExcelSheet();
+
+
+        }
+    //    private void CreateExcelSheet()
+    //    {
+    //        Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+    //        if (xlApp == null)
+    //        {
+    //            MessageBox.Show("Excel is not properly installed!!");
+    //            return;
+    //        }
+
+
+    //        Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+    //        Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+    //        object misValue = System.Reflection.Missing.Value;
+
+    //        xlWorkBook = xlApp.Workbooks.Add(misValue);
+    //        xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+    //        xlWorkSheet.Cells[1, 1] = "ID";
+    //        xlWorkSheet.Cells[1, 2] = "Name";
+    //        xlWorkSheet.Cells[2, 1] = "1";
+    //        xlWorkSheet.Cells[2, 2] = "One";
+    //        xlWorkSheet.Cells[3, 1] = "2";
+    //        xlWorkSheet.Cells[3, 2] = "Two";
+             
+
+
+    //        xlWorkBook.SaveAs("D:\\csharp-Excel.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+    //        xlWorkBook.Close(true, misValue, misValue);
+    //        xlApp.Quit();
+
+    //        //Marshal.ReleaseComObject(xlWorkSheet);
+    //        //Marshal.ReleaseComObject(xlWorkBook);
+    //        //Marshal.ReleaseComObject(xlApp);
+
+    //        MessageBox.Show("Excel file created , you can find the file D:\\csharp-Excel.xls");
+        
+    //}
+
+        private void Save(object sender, EventArgs e)
+        {
+            if (txtfileName.Text == "")
+            {
+                errorProvider1.SetError(txtfileName, "Please Enter Name");
+                //MessageBox.Show("Please enter File name");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(txtfileName, "");
+
+            }
+            if (rbtntextRows.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(rbtntextRows, "Please Select text Rows");
+                //MessageBox.Show("Please enter File name");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(rbtntextRows, "");
+
+            }
+            if (rbtntextColumns.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(rbtntextColumns, "Please Select text Columns");
+                //MessageBox.Show("Please enter File name");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(rbtntextColumns, "");
+
+            }
+            if (rbtnResultRows.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(rbtnResultRows, "Please Select Result Rows");
+                //MessageBox.Show("Please enter File name");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(rbtnResultRows, "");
+
+            }
+            if (rbtnResultColumns.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(rbtnResultColumns, "Please Select Result Columns");
+                //MessageBox.Show("Please enter File name");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(rbtnResultColumns, "");
+
+            }
+
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return;
+            }
+
+
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            ////xlWorkSheet.Cells[1, 1] = "ID";
+            ////xlWorkSheet.Cells[1, 2] = "Name";
+            ////xlWorkSheet.Cells[2, 1] = "1";
+            ////xlWorkSheet.Cells[2, 2] = "One";
+            ////xlWorkSheet.Cells[3, 1] = "2";
+            ////xlWorkSheet.Cells[3, 2] = "Two";
+            //xlWorkSheet.Cells[1, 1] = "SPY:US";
+
+            xlWorkSheet.Cells[int.Parse(rbtntextRows.SelectedItem.ToString()), int.Parse(rbtntextColumns.SelectedItem.ToString())] = "SPY:US";
+
+
+            //xlWorkSheet.Cells[1, 2] = "Name";
+            //xlWorkSheet.Cells[2, 1] = txtresult.Text;
+            xlWorkSheet.Cells[int.Parse(rbtnResultRows.SelectedItem.ToString()), int.Parse(rbtnResultColumns.SelectedItem.ToString())] = txtresult.Text;
+
+
+
+
+
+            xlWorkBook.SaveAs("E:\\"+txtfileName.Text+".xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            //Marshal.ReleaseComObject(xlWorkSheet);
+            //Marshal.ReleaseComObject(xlWorkBook);
+            //Marshal.ReleaseComObject(xlApp);
+
+            MessageBox.Show("Excel file created , you can find the file D:\\" + txtfileName.Text + ".xls");
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            lblfilename.Visible = false;
+            txtfileName.Visible = false;
+            btnSave.Visible = false;
+            txtresult.Text = "";
+            lblTextRows.Visible = false;
+            lblTextColumns.Visible = false;
+            lblResultRows.Visible = false;
+            lblResultColumn.Visible = false;
+
+            rbtntextRows.Visible = false;
+            rbtntextColumns.Visible = false;
+            rbtnResultRows.Visible = false;
+            rbtnResultColumns.Visible = false;
+        }
+    }
+}
